b19b8d154c6190c7f80ec5a14f02d6d07474291e
ilguy <yostr01@yahoo.com>
2022-05-21 13:24:32 -0500

    trying a different package

--- Data Import.R
+++ Data Import.R
@@ -1,11 +1,14 @@
 # Data Import
 library(tidyverse)
 library(pdftools)
+library(data.table)
+library(textreadr)
 
 path <- 'http://ecusd7.org/wp-content/uploads/bsk-pdf-manager/2021/09/EIS-Admin-and-Tchr-sal-and-beneifts-FY2021.pdf'
 
-data <- pdf_text(pdf = '2022_salaries.pdf') %>%
-  read_lines()
-view(data)
+data <- pdf_text(pdf = '2022_salaries.pdf')
 
-data <- as_data_frame(data)
+data2 <- read_pdf(path, remove.empty = TRUE, trim = TRUE)
+data2[1, "text"]
+text <- str_split(data2$text, "\n")
+text[[2]][1]
